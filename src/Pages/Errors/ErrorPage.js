668f05ed566f7f2e0f0c9e39fbe317e6b78300a1
Quentin <quentin.savean@gmail.com>
2019-04-03 23:55:30 +0200

    Error page

--- src/Pages/Errors/ErrorPage.js
+++ src/Pages/Errors/ErrorPage.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import './error.css';
 
 export default class Error extends Component {
   render() {
     return (
       <div>
-        <h1>Page introuvable</h1>
+        <h1 className="error">Page introuvable</h1>
       </div>
     )
   }
