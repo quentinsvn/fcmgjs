5d0097535dbe5c387684e6559590228b70b20b15
Quentin <quentin.savean@gmail.com>
2019-04-03 22:28:36 +0200

    Update icons

--- src/Components/TopNav/TopNav.js
+++ src/Components/TopNav/TopNav.js
@@ -1,25 +1,51 @@
 import React, { Component } from 'react'
-import { Input } from 'antd';
+import { Input, Popover } from 'antd';
 import './topnav.css';
 
 export default class TopNav extends Component {
   render() {
     const Search = Input.Search;
+    var content = new Object();
+    content.home = "Accueil";
+    content.scores = "Tableau des scores";
+    content.teams = "Teams"
+    content.events = "Evenements";
+    content.news = "Articles";
     return (
       <div className="TopNav">
          <header className="topnav_content">
               <div className="menu">
-                  <ul className="icons_menu"> 
-                      <li><Search 
+                  <ul className="icons_menu">  
+                      <li><Search  
                              placeholder="Rechercher..."
                              onSearch={value => console.log(value)}
                              style={{ width: 300, }}
-                        /></li>
-                      <li><i class="fas fa-home"></i></li>
-                      <li><i class="fas fa-table"></i></li>
-                      <li><i class="fas fa-users"></i></li>
-                      <li><i class="fas fa-exclamation-triangle"></i></li>
-                      <li><i class="fas fa-newspaper"></i></li>
+                        /></li>   
+                     <li>
+                        <Popover placement="bottom" content={content.home}>
+                          <span><i class="fas fa-home"></i></span>
+                        </Popover> 
+                    </li>
+                      <li>
+                        <Popover placement="bottom" content={content.scores}>
+                          <span><i class="fas fa-table"></i></span>
+                        </Popover>     
+                      </li>
+                      <li>
+                        <Popover placement="bottom" content={content.events}> 
+                          <span><i class="fas fa-users"></i></span>
+                        </Popover>      
+                      </li>
+                      <li>
+                        <Popover placement="bottom" content={content.teams}>   
+                          <span><i class="fas fa-exclamation-triangle"></i></span>
+                          </Popover>       
+                      </li>
+                      <li>
+                       <Popover placement="bottom" content={content.news}>     
+                          <span><i class="fas fa-newspaper"></i></span>
+                       </Popover>      
+                     </li>
                   </ul>
               </div>
           </header>
