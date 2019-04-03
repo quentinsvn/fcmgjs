4f84f4556b755b6765c0fc07c58824d64074f126
Quentin <quentin.savean@gmail.com>
2019-04-03 20:46:32 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { DatePicker, message } from "antd";
+import "antd/dist/antd.css";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+          <header className="topnav">
+              <div className="menu">
+                  <ul>
+                      <li>Accueil</li>
+                      <li>Tableau des scores</li>
+                      <li>Teams</li>
+                      <li>Evenements</li>
+                      <li>Articles</li>
+                  </ul>
+              </div>
+          </header>
       </div>
     );
   }
