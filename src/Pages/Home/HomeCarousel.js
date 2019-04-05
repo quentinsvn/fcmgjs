0d26401ba57535a6f06237e0b677cbe799eb3b8a
Quentin <quentin.savean@gmail.com>
2019-04-05 19:07:56 +0200

    Change timelaps of Carousel

--- src/Pages/Home/HomeCarousel.js
+++ src/Pages/Home/HomeCarousel.js
@@ -3,9 +3,13 @@ import { Carousel } from 'antd';
 
 export default class HomeCarousel extends Component {
   render() {
+    const settings = {
+      autoplay: true,
+      autoplaySpeed: 11000
+    };
     return (
       <div>  
-        <Carousel autoplay>
+        <Carousel ref={slider => (this.slider = slider)} {...settings}>
             <div><h3>Image 1</h3></div>
             <div><h3>Image 2</h3></div>
             <div><h3>Image 3</h3></div>
