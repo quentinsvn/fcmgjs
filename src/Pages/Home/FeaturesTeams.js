0ef2d1775bac75898e4e73e5a7f92791354ffaba
Quentin <quentin.savean@gmail.com>
2019-04-07 18:30:28 +0200

    Update FeaturesTeams.js

--- src/Pages/Home/FeaturesTeams.js
+++ src/Pages/Home/FeaturesTeams.js
@@ -12,6 +12,32 @@ export default class FeaturesTeams extends Component {
       slidesToShow: 3,
       slidesToScroll: 3,
       centerMode: true,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            initialSlide: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
     };
     return (
       <div className="container">
