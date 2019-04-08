b6707efc8b3c0f44e883125d5c16444887314991
Quentin <quentin.savean@gmail.com>
2019-04-08 12:12:21 +0200

    Add pictures

--- src/Pages/Home/FeaturesTeams.js
+++ src/Pages/Home/FeaturesTeams.js
@@ -6,11 +6,11 @@ export default class FeaturesTeams extends Component {
   render() {
     var settings = {
       dots: false,
-      infinite: true,
+      infinite: false,
       speed: 500,
-      slidesToShow: 3,
-      slidesToScroll: 3,
-      centerMode: true,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      centerMode: false,
       responsive: [
         {
           breakpoint: 1024,
